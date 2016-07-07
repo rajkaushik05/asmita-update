73369cb168f2fdee17a3efbf2dba7b4880c62869
rajkaushik05 <rajbir.kaushik@srijan.in>
2016-07-07 17:48:53 +0530

    Index page stucture done

---
+++ js/main.js
@@ -0,0 +1,23 @@
+"use strict"
+
+$(document).ready(function(){
+	$("input").bind("input", function() {
+    var fields = $(this).parents('form:eq(0),body').find('input');
+        var index = fields.index( this );
+        //alert(index);
+        //alert(fields.val().length);
+        //alert(fields.attr("maxlength"));
+        	if ( index > -1 && fields.val().length >= parseInt(fields.attr("maxlength"),10)) {
+            	fields.eq( index + 1 ).focus();
+        	}
+        	return false;
+	});
+});
+
+
+
+
+
+
+
+
